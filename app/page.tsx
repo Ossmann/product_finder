1044bea9210fc67506c1908bc1cafad71edadcbb
Jakob Ossmann <Jakobossmann@gmail.com>
2024-04-24 13:53:59 +1000

    landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,59 @@
 import Image from "next/image";
 import RoundedButton from "../app/ui/buttons";
+import {
+  AdjustmentsHorizontalIcon
+} from '@heroicons/react/24/outline';
 
 export default function Home() {
   return (
     <main className="relative min-h-screen flex flex-col items-center justify-between p-24">
 
-      {/* Background image */}
-      <div className="absolute inset-0 z-[-1]">
-        <Image
-          src="/banner-img1-CSG.png"
-          layout="fill"
-          objectFit="cover"
-          quality={100}
-          alt="Background Image"
-        />
-      </div>
+    {/* Dark Topbar to make logo better visible */}
+    <div className="absolute top-0 left-0 w-full h-60 bg-gradient-to-b from-black to-transparent">
+    </div>
 
-      {/* Dark overlay */}
-      <div className="absolute inset-0 bg-black opacity-30 z-[-1]"></div>
-
-      <div className="z-10 w-full max-w-5xl items-center justify-between lg:flex">
-
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          This is a uni project&nbsp;
-          <code className="font-bold">In progress</code>
-        </p>
-
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0">
-            <Image
-              src="/logo_CSG.svg"
-              alt="CSG Logo"
-              className="dark:invert"
-              width={200}
-              height={76}
-              priority
-            />
-          </a>
+        {/* Background image */}
+        <div className="absolute inset-0 z-[-1]">
+          <Image
+            src="/banner-img1-CSG.png"
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+            alt="Background Image"
+          />
         </div>
-      </div>
+
+        {/* Dark overlay */}
+        <div className="absolute inset-0 bg-black opacity-20 z-[-1]"></div>
+
+        <div className="z-10 w-full max-w-5xl items-center justify-between lg:flex">
+
+        {/* CSG LOGO top left */}
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+            <a className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0">
+              <Image
+                src="/logo_CSG.svg"
+                alt="CSG Logo"
+                className="dark:invert"
+                width={200}
+                height={76}
+                priority
+              />
+            </a>
+          </div>
+
+          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 font-normal text-black">
+              This is a uni project - In progress
+          </p>
+
+        </div>
+
+        
+      
 
       <div className="relative z-10 flex flex-col items-center justify-center text-center">
-        <p>WELCOME AUSTRALIA</p>
-        <p>Find the right tool to tell your culture story</p>
+        <p className="uppercase font-bold text-2xl">Welcome Australia</p>
+        <p>Find the right tool to tell your culture story.</p>
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
@@ -51,8 +62,16 @@ export default function Home() {
 
       {/* Link to start the Product Finder */}
       <div>
-        <RoundedButton href="/questions">START</RoundedButton>
+        <RoundedButton icon={<AdjustmentsHorizontalIcon className="w-5 h-5" />} href="/questions">
+          Start Finder
+        </RoundedButton>
       </div>
+
+      {/* Bottom gradiant */}
+      <div className="absolute bottom-0 left-0 w-full h-20">
+      <div className="absolute inset-0 bg-gradient-to-t to-transparent">
+      </div>
+    </div>
 
     </main>
   );
--- app/ui/buttons.tsx
+++ app/ui/buttons.tsx
@@ -3,12 +3,14 @@ import Link from 'next/link';
 interface RoundedButtonProps {
   href: string;
   children: React.ReactNode;
+  icon?: React.ReactNode;
 }
 
-const RoundedButton: React.FC<RoundedButtonProps> = ({ href, children }) => {
+const RoundedButton: React.FC<RoundedButtonProps> = ({ href, children, icon }) => {
   return (
     <Link href={href}>
-      <div className="inline-block px-6 py-3 border border-orange-500 rounded-full text-orange-500 hover:bg-orange-500 hover:text-white transition duration-300 ease-in-out">
+      <div className="inline-flex items-center px-6 py-3 bg-white border border-orange-500 rounded-full text-orange-500 hover:bg-orange-500 hover:text-white transition duration-300 ease-in-out uppercase">
+        {icon && <span className="mr-2">{icon}</span>}
         {children}
       </div>
     </Link>
